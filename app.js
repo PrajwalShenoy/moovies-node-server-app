bd741f5a6ff6a100772c98c3303f505817dd2fab
rakshitsoni05 <rakshitsoni5@gmail.com>
2023-12-08 17:56:50 -0500

    mongodb

--- app.js
+++ app.js
@@ -1,8 +1,23 @@
 import express from 'express'
 import Hello from "./hello.js"
 import UsersRoutes from "./Users/routes.js";
-
+import mongoose from "mongoose";
 import cors from "cors";
+import "dotenv/config";
+
+const CONNECTION_STRING = process.env.DB_CONNECTION_STRING;
+
+mongoose
+    .connect(CONNECTION_STRING)
+    .then(() => {
+        console.log("Connected to MongoDB");
+    })
+    .catch((error) => {
+        console.error("Error connecting to MongoDB:", error);
+    })
+
+
+
 const app = express();
 app.use(cors());
 app.use(express.json());
